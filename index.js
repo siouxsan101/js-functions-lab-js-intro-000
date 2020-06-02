53c0cb4e8f658901246a1a0570c408e817966cdf
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-02 17:38:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 function happyHolidays(){
   return "Happy holidays!";
 }
+
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
+
 function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!"``
+  return `Happy ${holiday}, ${name}!`
 }
